a9d5b69f1fa7657f6db11fde1ac1c678acdbf4ab
Kenny Bass <kenny_bass@outlook.com>
2022-05-09 16:23:55 -0500

    New Logo

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -117,9 +117,13 @@ export function getHtml(parsedReq: ParsedRequest) {
         <div>
             <div class="spacer">
             <div class="logo-wrapper">
-                ${images.map((img, i) =>
-                    getPlusSign(i) + getImage(img, widths[i], heights[i])
-                ).join('')}
+                <img
+                    class="logo"
+                    alt="Generated Image"
+                    src="/public/logo.png"
+                    width="auto"
+                    height="225"
+                />
             </div>
             <div class="spacer">
             <div class="heading">${emojify(
@@ -129,18 +133,4 @@ export function getHtml(parsedReq: ParsedRequest) {
         </div>
     </body>
 </html>`;
-}
-
-function getImage(src: string, width ='auto', height = '225') {
-    return `<img
-        class="logo"
-        alt="Generated Image"
-        src="${sanitizeHtml(src)}"
-        width="${sanitizeHtml(width)}"
-        height="${sanitizeHtml(height)}"
-    />`
-}
-
-function getPlusSign(i: number) {
-    return i === 0 ? '' : '<div class="plus">+</div>';
 }
