9eeda1822599af7c4873fd262f399046a0b5df9d
Kenny Bass <kenny_bass@outlook.com>
2022-05-10 08:05:25 -0500

    Update Style

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -3,32 +3,21 @@ import { readFileSync } from 'fs';
 import { sanitizeHtml } from './sanitizer';
 import { ParsedRequest } from './types';
 
-const rglr = readFileSync(`${__dirname}/../_fonts/Inter-Regular.woff2`).toString('base64');
-const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString('base64');
-const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString('base64');
+const rglr = readFileSync(`${__dirname}/../_fonts/Nunito-VariableFont-wght.ttf`).toString('base64');
+const main = readFileSync(`${__dirname}/../_fonts/Letter_Magic.ttf`).toString('base64');
+
 
 function getCss() {
     return `
     @font-face {
-        font-family: 'Inter';
-        font-style:  normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
+        font-family: 'Nunito';
+        src: url(data:font/ttf;charset=utf-8;base64,${rglr}) format('ttf');
     }
 
     @font-face {
-        font-family: 'Inter';
-        font-style:  normal;
-        font-weight: bold;
-        src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
+        font-family: 'Magic';
+        src: url(data:font/ttf;charset=utf-8;base64,${main}) format('ttf');
     }
-
-    @font-face {
-        font-family: 'Vera';
-        font-style: normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${mono})  format("woff2");
-      }
 
     body {
         background: #f1f5f9;
@@ -38,38 +27,45 @@ function getCss() {
         text-align: center;
         align-items: center;
         justify-content: center;
+        font-family: 'Nunito', sans-serif;
+    }
+
+    .wrapper {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
     }
 
     .logo-wrapper {
-        display: flex;
-        align-items: center;
-        align-content: center;
-        justify-content: center;
-        justify-items: center;
-        padding-bottom: 2rem;
+        padding: 2rem;
     }
 
-    .logo {
-        margin: 0 75px;
-    }
-    
-    .heading {
-        font-family: 'Inter', sans-serif;
-        color: black;
-    }
-
-    .heading .title {
-        margin: 0;
-        padding: 0;
+    .logo-text {
         font-size: 1.5rem;
-        font-weight: bold;
+        font-family: 'Magic', sans-serif;
     }
 
-    .heading .code {
+    .code-wrapper {
+        padding: 2rem;
+        text-align: left;
+    }
+
+    .code-wrapper h1, .code-wrapper p {
         margin: 0;
         padding: 0;
-        font-size: 7rem;
+    }
+
+    .code-label {
+        font-weight: 600;
+        font-size: 1.25rem;
+    }
+
+    .code-text {
         font-weight: bold;
+        font-size: 10rem;
     }`;
 }
 
@@ -84,7 +80,7 @@ export function getHtml(parsedReq: ParsedRequest) {
         ${getCss()}
     </style>
     <body>
-        <div>
+        <div class="wrapper">
             <div class="logo-wrapper">
                 <img
                     class="logo"
@@ -92,13 +88,12 @@ export function getHtml(parsedReq: ParsedRequest) {
                     src="https://www.feastingtogether.com/Branding/Logo.png"
                     width="auto"
                     height="225"
-                />
+                    />
+                <p class="logo-text">Feasting Together</p>
             </div>
-            <div class="heading">
-            <p class="title">Invite Code</p>
-            <p class="code">
-                ${sanitizeHtml(text)}
-            </p>
+            <div class="code-wrapper">
+                <p class="code-label">Invite Code:</p>
+                <h1 class="code-text">${sanitizeHtml(text)}</h1>
             </div>
         </div>
     </body>
